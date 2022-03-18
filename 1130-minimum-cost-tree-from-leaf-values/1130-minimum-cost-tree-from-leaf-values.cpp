b0d0c4247ace9dc6392977c71d9a68adaed66c64
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-03-18 17:29:27 +0530

    Time: 3 ms (84.88%), Space: 8.3 MB (66.82%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,21 +1,21 @@
 class Solution {
-    pair<int,int> dp[41][41];
-    pair<int,int> backtrack(int start, int end, vector<int>& arr){
-        if(start==end) return dp[start][end] = {arr[start],0};
-        if(dp[start][end].first != -1) return dp[start][end];
-        int nonleafsum = INT_MAX;
-        int maxleaf = 0;
-        for(int i = start; i < end; i++){
-            auto left = backtrack(start,i,arr);
-            auto right = backtrack(i+1,end,arr);
-            nonleafsum = min(nonleafsum,left.first*right.first+left.second+right.second);
-            maxleaf = max({maxleaf,left.first,right.first});
-        }
-        return dp[start][end] = {maxleaf,nonleafsum};
-    }
+    
 public:
     int mctFromLeafValues(vector<int>& arr) {
-        for(int i=0;i<41;i++) for(int j=0;j<41;j++) dp[i][j]={-1,-1};
-        return backtrack(0,arr.size()-1,arr).second;
+        vector<int> v;
+        int sum=0;
+        for(int i=0;i<arr.size();i++){
+            while(v.size()>0&&v.back()<=arr[i]){
+                if(v.size()>1&&v[v.size()-2]<arr[i])
+                    sum += v[v.size()-1]*v[v.size()-2];
+                else
+                    sum += v.back()*arr[i];
+                v.pop_back();
+            }
+            v.push_back(arr[i]);
+        }
+        for(int i=v.size()-2;i>=0;i--)
+            sum += v[i]*v[i+1];
+        return sum;
     }
 };
