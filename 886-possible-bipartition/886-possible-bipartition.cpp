231f3c536c3950bc45c505a13747dd18df4c32ed
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-04-12 21:40:41 +0530

    Time: 204 ms (85.44%), Space: 72.6 MB (17.73%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,17 +1,26 @@
 class Solution {
-    bool dfs(vector<vector<int>>&adj, vector<bool>&color, vector<bool>&visit, int node, bool fg){
-        if(visit[node] && color[node]==fg) return true;
-        if(visit[node] && color[node]!=fg) return false;
-        visit[node] = true;
-        color[node] = fg;
-        for(int x : adj[node]){
-            if(!dfs(adj,color,visit,x,!fg)) return false;
-        }
-        return true;
-    }
+   bool bfs(vector<vector<int>>&adj, vector<int>&color, vector<bool>&visit, int node){
+       queue<int> qt;
+       qt.push(node);
+       color[node] = 1;
+       while(!qt.empty()){
+            int n = qt.front();
+            qt.pop();
+            if(visit[n]) continue;
+            visit[n] = true;
+            for(int x : adj[n]){
+                if(color[n]==color[x]) return false;
+                if(color[n]==1) color[x] = 2;
+                else color[x] = 1;
+                qt.push(x);
+            }
+       }
+       return true;
+   }
 public:
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        vector<bool> color(n+1,false), visit(n+1,false);
+        vector<bool> visit(n+1,false);
+        vector<int> color(n+1,0);
         vector<vector<int>> adj(n+1);
         for(auto x : dislikes){
             adj[x[0]].push_back(x[1]);
@@ -19,7 +28,7 @@ public:
         }
         for(int i=1;i<=n;i++){
             if(visit[i]) continue;
-            if(!dfs(adj,color, visit,i, 1)) return false;
+            if(!bfs(adj,color, visit,i)) return false;
         }
         return true;
     }
