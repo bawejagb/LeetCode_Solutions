e73a93301e1db7cc5e35477dd7452a278e4924d1
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-04-13 15:08:35 +0530

    Time: 68 ms (76.03%), Space: 20.7 MB (28.58%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,32 +1,36 @@
 class Solution {
+    map<int,vector<int>> row,col;
+    void dfs(int id, vector<bool>&visit, vector<vector<int>>&stones){
+        if(visit[id]) return;
+        visit[id] = true;
+        int x,y;
+        x = stones[id][0];
+        y = stones[id][1];
+        for(int a : row[x])
+            dfs(a,visit,stones);
+        for(int b : col[y])
+            dfs(b,visit,stones);
+        }
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        vector<bool> del(n,false),visit(n,false);
-        queue<int>qt;
-        for(int j=0;j<n;j++){
-            if(visit[j]) continue;
-            qt.push(j);
-            while(!qt.empty()){
-                int p = qt.front();
-                qt.pop();
-                int x,y;
-                visit[p] = true;
-                x = stones[p][0];
-                y = stones[p][1];
-                for(int i=0;i<n;i++){
-                    if(visit[i]) continue;
-                    if(stones[i][0]==x||stones[i][1]==y){
-                        qt.push(i);
-                        del[i] = true;
-                    }
-                }
-            }
+        vector<bool> visit(n,false);
+        
+        for(int i=0;i<n;i++){
+            int x,y;
+            x = stones[i][0];
+            y = stones[i][1];
+            row[x].push_back(i);
+            col[y].push_back(i);
         }
         int res=0;
-        for(int i=0;i<n;i++){
-            if(del[i]) res++;
+        for(int j=0;j<n;j++){
+            if(!visit[j]){
+                dfs(j,visit,stones);
+                res++;
+            }
+            
         }
-        return res;
+        return n-res;
     }
 };
