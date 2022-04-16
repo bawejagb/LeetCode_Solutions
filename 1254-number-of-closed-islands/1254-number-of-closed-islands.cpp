0f64474ed30be6fd87bc04f01b7dc2bf0c1fd4e2
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-04-16 21:49:50 +0530

    Time: 7 ms (98.79%), Space: 9.5 MB (36.11%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,48 +1,41 @@
 class Solution {
+    void dfs(vector<vector<int>>&grid,int i, int j,int &flag){
+        if(i<1 || j<1||i>=grid.size()-1|| j>=grid[0].size()-1)
+        {
+            if(i==0 || j==0||i==grid.size()-1 ||j==grid[0].size()-1)
+                if(grid[i][j]==0)
+                    flag=1;
+            return;
+        }
+        if(grid[i][j]==1 || grid[i][j]==2)
+            return;
+        
+        grid[i][j]=2;
+        
+        dfs(grid,i+1,j,flag);
+        dfs(grid,i-1,j,flag);
+        dfs(grid,i,j+1,flag);
+        dfs(grid,i,j-1,flag);
+    }
 public:
     int closedIsland(vector<vector<int>>& grid) {
-        int res=0;
+        int cnt =0;
         int n = grid.size();
         int m = grid[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if((i==0||j==0||i==n-1||j==m-1)&&grid[i][j]==0){
-                    queue<pair<int,int>> qt;
-                    qt.push({i,j});
-                    while(!qt.empty()){
-                        auto p = qt.front();
-                        qt.pop();
-                        int a = p.first;
-                        int b = p.second;
-                        grid[a][b] = 2;
-                        if(a>0&&grid[a-1][b]==0) qt.push({a-1,b});
-                        if(a<n-1&&grid[a+1][b]==0) qt.push({a+1,b});
-                        if(b>0&&grid[a][b-1]==0) qt.push({a,b-1});
-                        if(b<m-1&&grid[a][b+1]==0) qt.push({a,b+1});
-                    }
+        for(int i=1;i<n-1;i++)
+        {
+            for(int j=1;j<m-1;j++)
+            {
+                int flag=0;
+                if(grid[i][j]==0)
+                {
+                    dfs(grid,i,j,flag);
+                    if(flag==0)
+                        cnt++;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==0){
-                    res++;
-                    queue<pair<int,int>> qt;
-                    qt.push({i,j});
-                    while(!qt.empty()){
-                        auto p = qt.front();
-                        qt.pop();
-                        int a = p.first;
-                        int b = p.second;
-                        grid[a][b] = 2;
-                        if(a>0&&grid[a-1][b]==0) qt.push({a-1,b});
-                        if(a<n-1&&grid[a+1][b]==0) qt.push({a+1,b});
-                        if(b>0&&grid[a][b-1]==0) qt.push({a,b-1});
-                        if(b<m-1&&grid[a][b+1]==0) qt.push({a,b+1});
-                    }
-                }
-            }
-        }
-        return res;
+        
+        return cnt;
     }
 };
