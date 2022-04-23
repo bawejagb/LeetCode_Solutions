f54222782db1ca5c2451a5e32ca96103ab4f2620
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-04-23 22:13:52 +0530

    Time: 1950 ms (5.10%), Space: 394.3 MB (5.73%) - LeetHub

--- 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.cpp
+++ 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.cpp
@@ -1,31 +1,28 @@
 class TweetCounts {
-public:
-     map <string, multiset <int>> tweetTimeline;
-    unordered_map <string, int> freqEnum;
+    map<string,multiset<int>> tmap;
 public:
     TweetCounts() {
-        freqEnum["minute"] = 60;
-        freqEnum["hour"] = 60 * freqEnum["minute"];
-        freqEnum["day"] = 60 * freqEnum["hour"];
     }
     
     void recordTweet(string tweetName, int time) {
-        tweetTimeline[tweetName].insert(time);
+        tmap[tweetName].insert(time);
     }
     
     vector<int> getTweetCountsPerFrequency(string freq, string tweetName, int startTime, int endTime) {
-        int intervals = ((endTime - startTime) /  freqEnum[freq]) + 1;
-        vector <int> tweetCountsPerFrequency(intervals, 0);
-        
-        auto tweetClosestToStartTimeItr = tweetTimeline[tweetName].lower_bound(startTime);   
-        auto tweetClosestToEndTimeItr = tweetTimeline[tweetName].upper_bound(endTime);
-        
-        for(auto it = tweetClosestToStartTimeItr; it != tweetClosestToEndTimeItr; it++) {
-            int idx = (*it - startTime) / freqEnum[freq]; // see which frequency interval my current time lies in
-            tweetCountsPerFrequency[idx]++;
+        int div;
+        if(freq[0] == 'm') div = 60;
+        else if(freq[0] == 'h') div = 3600;
+        else div = 86400;
+        auto vec = tmap[tweetName];
+        int num = (endTime-startTime)/div;
+        vector<int> res(num+1,0);
+        auto start = vec.lower_bound(startTime);
+        auto end = vec.upper_bound(endTime);
+        while(start != end && *start<= endTime){
+            res[(*start-startTime)/div]++;
+            start++;
         }
-        
-        return tweetCountsPerFrequency;
+        return res;
     }
 };
 
