724f28c48dc1b0438b8971bbfb3028d971f2feab
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-04-24 17:31:30 +0530

    Time: 169 ms (49.13%), Space: 48.8 MB (75.44%) - LeetHub

--- 432-all-oone-data-structure/432-all-oone-data-structure.cpp
+++ 432-all-oone-data-structure/432-all-oone-data-structure.cpp
@@ -1,5 +1,5 @@
 class AllOne {
-    map<int, set<string>> hmap;
+    set<pair<int,string>> hset;
     unordered_map<string,int> smap;
     int mn, mx;
 public:
@@ -10,36 +10,30 @@ public:
     void inc(string key) {
         smap[key]++;
         if(smap[key]>1){
-            if(hmap[smap[key]-1].size()==1)
-                hmap.erase(smap[key]-1);
-            else
-                hmap[smap[key]-1].erase(key);
+            hset.erase({smap[key]-1,key});
         }
-        hmap[smap[key]].insert(key);
+        hset.insert({smap[key],key});
     }
     
     void dec(string key) {
-        if(hmap[smap[key]].size()==1)
-            hmap.erase(smap[key]);
-        else
-            hmap[smap[key]].erase(key);
+        hset.erase({smap[key],key});
         smap[key]--;
         if(smap[key]==0){
             smap.erase(key);
         }
         else
-            hmap[smap[key]].insert(key);
+            hset.insert({smap[key],key});
         
     }
     
     string getMaxKey() {
         if(smap.empty()) return "";
-        return *(hmap.rbegin()->second).begin();
+        return (hset.rbegin()->second);
     }
     
     string getMinKey() {
         if(smap.empty()) return "";
-        return *(hmap.begin()->second).begin();
+        return (hset.begin()->second);
     }
 };
 
