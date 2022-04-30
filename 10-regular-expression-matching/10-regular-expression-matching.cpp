fdd3b2292faf8fe922dbdd855ae2c81f3dcb5216
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-04-30 17:54:04 +0530

    Time: 31 ms (31.67%), Space: 21.3 MB (7.30%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,19 +1,18 @@
 class Solution {
-    unordered_map<string,bool> dp;
-    bool dfs(int i, int j, string s, string p){
-        string pr = to_string(i)+" "+to_string(j);
-        if(dp.find(pr)!=dp.end()) return dp[pr];
-        if(i>=s.size()&&j>=p.size()) return dp[pr] = true;
-        if(j>= p.size()) return dp[pr] = false;
+    bool dfs(int i, int j, string s, string p,vector<vector<int>>&dp){
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i>=s.size()&&j>=p.size()) return dp[i][j] = 1;
+        if(j>= p.size()) return dp[i][j] = 0;
         bool match = i<s.size()&&(s[i]==p[j] || p[j]=='.');
         if((j+1)<p.size() && p[j+1]=='*')
-            return dp[pr] = (dfs(i,j+2,s,p)||(match&&dfs(i+1,j,s,p)));
+            return dp[i][j] = (dfs(i,j+2,s,p,dp)||(match&&dfs(i+1,j,s,p,dp)));
         if(match)
-            return dp[pr] = dfs(i+1,j+1,s,p);
-        return dp[pr] = false;
+            return dp[i][j] = dfs(i+1,j+1,s,p,dp);
+        return dp[i][j] = 0;
     }
 public:
     bool isMatch(string s, string p) {
-        return dfs(0,0,s,p);
+        vector<vector<int>> dp(s.size()+1,vector<int>(p.size()+1,-1));
+        return dfs(0,0,s,p,dp);
     }
 };
