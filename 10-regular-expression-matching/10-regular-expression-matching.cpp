3778c6f5a06c70ef142ce6d611f7e877b69b22fc
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-04-30 18:21:36 +0530

    Time: 12 ms (45.60%), Space: 7.1 MB (47.46%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,18 +1,18 @@
 class Solution {
-    bool dfs(int i, int j, string s, string p,vector<vector<int>>&dp){
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i>=s.size()&&j>=p.size()) return dp[i][j] = 1;
-        if(j>= p.size()) return dp[i][j] = 0;
-        bool match = i<s.size()&&(s[i]==p[j] || p[j]=='.');
-        if((j+1)<p.size() && p[j+1]=='*')
-            return dp[i][j] = (dfs(i,j+2,s,p,dp)||(match&&dfs(i+1,j,s,p,dp)));
-        if(match)
-            return dp[i][j] = dfs(i+1,j+1,s,p,dp);
-        return dp[i][j] = 0;
-    }
 public:
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.size()+1,vector<int>(p.size()+1,-1));
-        return dfs(0,0,s,p,dp);
+        vector<vector<bool>> dp(s.size()+1,vector<bool>(p.size()+1,0));
+        dp[s.size()][p.size()] = true;
+        for(int i=s.size(); i >= 0;i--){
+            for(int j=p.size()-1; j >= 0;j--){
+                bool match = (i < s.size() && (p[j] == s[i] || p[j] == '.'));
+                if(j+1 < p.size() && p[j+1] == '*')
+                    dp[i][j] = dp[i][j+2] || (match && dp[i+1][j]);
+                else
+                    dp[i][j] = match && dp[i+1][j+1];
+            }
+        }
+        return dp[0][0];
     }
+    
 };
