0488177c6ea7ac247f6e4f284a456b9188a4f5bf
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-05-01 15:06:11 +0530

    Time: 63 ms (50.87%), Space: 12 MB (95.41%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -1,33 +1,37 @@
 class Solution {
+    int col[500][2];
+    int dp[500];
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int m,n,res=0;
-        m = matrix.size();
-        n = matrix[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,0));
-        for(int i=0;i<m;i++){
-            stack<int> st;
-            int idx, val;
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]=='1'){
-                    if(i==0) dp[i][j] = 1;
-                    else dp[i][j] = dp[i-1][j]+1;
+        int m=matrix.size();
+        if(!m) return 0;
+        int n=matrix[0].size();
+        if(!n) return 0;
+        int ans=0;
+        for(int i=0; i<m; ++i) {
+            dp[i]=0;
+        }
+        for(int i, j=0, k, l, w; j<n; ++j) {
+            k=0;
+            col[0][0]=0;
+            col[0][1]=-1;
+            for(i=m-1; i>=0; --i) {
+                w=dp[i]=matrix[i][j]=='0'? 0:dp[i]+1;
+                for(l=k; l>=0 && col[l][0]>w; --l) {
+                    ans=max(ans, col[l][0]*(col[l][1]-i));
                 }
-                while(!st.empty()&&dp[i][j]<dp[i][st.top()]){
-                    idx = st.top();
-                    st.pop();
-                    val = dp[i][idx]*(st.empty()?j:j-1-st.top());
-                    res = max(res,val);
+                if(++l>k) {
+                    col[l][0]=w;
+                    col[l][1]=i;
+                } else {
+                    col[l][0]=w;
                 }
-                st.push(j);
+                k=l;
             }
-            while(!st.empty()){
-                idx = st.top();
-                st.pop();
-                val = dp[i][idx]*(st.empty()?n:n-1-st.top());
-                res = max(res,val);
+            for(; k>=0; --k) {
+                ans=max(ans, col[k][0]*(col[k][1]+1));
             }
         }
-        return res;
+        return ans;
     }
 };
