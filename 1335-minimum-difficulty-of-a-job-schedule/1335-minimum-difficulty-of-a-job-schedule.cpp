e547781280a8fe89f0332c4f71b5f338f0db2a4d
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-05-04 16:43:51 +0530

    Time: 29 ms (86.78%), Space: 7.2 MB (99.17%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,26 +1,28 @@
 class Solution {
-    vector<int> mxfar;
-    int dfs(vector<int>& jd, int d, int idx, vector<vector<int>>&dp){
-        int n = jd.size();
-        if(d==1) return mxfar[idx];
-        if(dp[d][idx]!=-1) return dp[d][idx];
-        int ans = 1e9;
-        int mxVal = 0;
-        for(int i = idx; i< n-d+1;i++){
-            mxVal = max(mxVal, jd[i]);
-            ans = min(ans, mxVal+dfs(jd, d-1, i+1,dp));
-        }
-        return dp[d][idx]=ans;
-    }
+
 public:
     int minDifficulty(vector<int>& jobDifficulty, int d) {
         int n = jobDifficulty.size();
+        
         if(n<d) return -1;
-        mxfar.resize(n);
-        mxfar[n-1] = jobDifficulty[n-1];
-        for(int i=n-2;i>=0;i--)
-            mxfar[i] = max(jobDifficulty[i],mxfar[i+1]);
-        vector<vector<int>> dp(d+1,vector<int>(n,-1));
-        return dfs(jobDifficulty, d, 0,dp);
+        
+        int dp[d][n];
+        
+        dp[0][0] = jobDifficulty[0];
+        for(int i=1;i<n;i++)
+            dp[0][i] = max(dp[0][i-1],jobDifficulty[i]);
+        
+        for(int i=1;i<d;i++){
+            for(int j=i;j<n;j++){
+                int mxLocal = jobDifficulty[j];
+                int mnVal = INT_MAX;
+                for(int k=j;k>=i;k--){
+                   mxLocal = max(mxLocal, jobDifficulty[k]);
+                    mnVal = min(mnVal, dp[i-1][k-1]+mxLocal);
+                }
+                dp[i][j] = mnVal;
+            }
+        }
+        return dp[d-1][n-1];
     }
 };
