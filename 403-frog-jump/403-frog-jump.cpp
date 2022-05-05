ab2be280368005737d7e1e6ef528f15e24fb3086
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-05-05 17:55:22 +0530

    Time: 322 ms (36.57%), Space: 46.2 MB (51.02%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canCross(vector<int>& stones) {
+        unordered_map<int,set<int>> hmap;
+        int n = stones.size();
+        for(int x : stones){
+            hmap[x].insert(0);
+        }
+        int steps;
+        if(stones[1]==1)
+            hmap[stones[1]].insert(1);
+        else return false;
+        for(int i = 1; i < n-1; i++){
+            for(auto j=++hmap[stones[i]].begin();j!=hmap[stones[i]].end();j++){
+                steps = *j;
+                if(steps>1&&hmap[stones[i]+steps-1].size()>0)
+                    hmap[stones[i]+steps-1].insert(steps-1);
+                if(hmap[stones[i]+steps].size()>0)
+                    hmap[stones[i]+steps].insert(steps);
+                if(hmap[stones[i]+steps+1].size()>0)
+                    hmap[stones[i]+steps+1].insert(steps+1);
+            }
+        }
+        return hmap[stones[n-1]].size()>1;
+    }
+};
