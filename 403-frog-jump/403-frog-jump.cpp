7da6e1ab19ce986b7d6dfde3d96669683d80573b
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-05-05 18:22:27 +0530

    Time: 45 ms (98.88%), Space: 16.1 MB (96.78%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
+    unordered_map<int, bool>dp;
     bool canCross(vector<int>& stones) {
-        unordered_map<int,set<int>> hmap;
-        int n = stones.size();
-        for(int x : stones){
-            hmap[x].insert(0);
-        }
-        int steps;
-        if(stones[1]==1)
-            hmap[stones[1]].insert(1);
-        else return false;
-        for(int i = 1; i < n; i++){
-            for(auto j=++hmap[stones[i]].begin();j!=hmap[stones[i]].end();j++){
-                steps = *j;
-                if(steps>1&&hmap[stones[i]+steps-1].size()>0)
-                    hmap[stones[i]+steps-1].insert(steps-1);
-                if(hmap[stones[i]+steps].size()>0)
-                    hmap[stones[i]+steps].insert(steps);
-                if(hmap[stones[i]+steps+1].size()>0)
-                    hmap[stones[i]+steps+1].insert(steps+1);
-                if(hmap[stones[n-1]].size()>1) return true;
-            }
-        }
-        return false;
+        return dfs(stones, 0, 0);
+    }
+    
+    bool dfs(vector<int>&stones, int pos, int k) {
+        int key = pos | k << 11;
+        
+        if(dp.find(key) != dp.end())
+            return dp[key];
+        
+        for(int i=pos+1; i<stones.size(); i++) {
+            int gap = stones[i] - stones[pos];
+            if(gap < k-1) continue;
+            if(gap > k+1) return dp[key] = false;
+            if( dfs(stones, i, gap) ) 
+                return dp[key] = true;
+        } 
+        
+        return dp[key] = pos == stones.size()-1;
     }
 };
