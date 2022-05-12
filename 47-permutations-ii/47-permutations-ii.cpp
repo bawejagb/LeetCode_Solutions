064ca3e4c1a147d8e3b64b732f25f2a0a571a06d
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-05-12 16:23:31 +0530

    Time: 8 ms (74.77%), Space: 8.2 MB (98.84%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,30 +1,44 @@
-class Solution {
-    vector<vector<int>> res;
-    void backTrack(vector<int>&nums, vector<bool>&visit, vector<int>&path){
-        if(path.size()==nums.size()){
-            res.push_back(path);
-            return;
-        }
-        unordered_set<int> hset;
-        for(int i=0;i<nums.size();i++){
-            if(hset.find(nums[i])!=hset.end()) continue;
-            if(!visit[i]){
-                hset.insert(nums[i]);
-                visit[i]=1;
-                path.push_back(nums[i]);
-                backTrack(nums, visit, path);
-                visit[i]=0;
-                path.pop_back();
+class Solution
+{
+
+    void nextPermutation(vector<int> &vec)
+    {
+        int n = vec.size();
+        if (n == 1) return;
+        int breakPoint = -1;
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (vec[i] < vec[i + 1])
+            {
+                breakPoint = i;
+                break;
             }
         }
-        
+        if (breakPoint == -1)
+        {
+            reverse(vec.begin(), vec.end());
+            return;
+        }
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (vec[i] > vec[breakPoint])
+            {
+                swap(vec[i], vec[breakPoint]);
+                break;
+            }
+        }
+        reverse(vec.begin() + breakPoint + 1, vec.end());
     }
-public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int n=nums.size();
-        vector<bool> visit(n,0);
-        vector<int> path;
-        backTrack(nums,visit,path);
-        return res;
-    }
+    public:
+        vector<vector < int>> permuteUnique(vector<int> &nums)
+        {
+            vector<int> vec(nums);
+            vector<vector < int>> res;
+            do {
+                res.push_back(vec);
+                nextPermutation(vec);
+            }
+            while (vec != nums);
+            return res;
+        }
 };
