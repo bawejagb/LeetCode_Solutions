41ddb817818a972106e7cd415f8c44f1e104df17
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-05-22 07:22:23 +0530

    Time: 3 ms (91.94%), Space: 6.1 MB (96.68%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -4,15 +4,16 @@ class Solution
         int countSubstrings(string s)
         {
             int count = 0;
-            int left, right;
-            for (int i = 0; i < s.size(); i++)
+            int left, right, n;
+            n = s.size();
+            for (int i = 0; i < n; i++)
             {
                 left = right = i;
-                while (left >= 0 && right < s.size() && s[left] == s[right])
+                while (left >= 0 && right < n && s[left] == s[right])
                     count++, left--, right++;
 
                 left = i - 1, right = i;
-                while (left >= 0 && right < s.size() && s[left] == s[right])
+                while (left >= 0 && right < n && s[left] == s[right])
                     count++, left--, right++;
             }
             return count;
