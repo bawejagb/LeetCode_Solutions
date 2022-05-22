4e2b1265d21299793f25384cb1204c29d32ae4c6
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-05-22 07:18:27 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (82.67%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -6,24 +6,15 @@ class Solution
             int count = 0;
             int left, right, n;
             n = s.size();
-            
             for (int i = 0; i < n; i++)
             {
                 left = right = i;
                 while (left >= 0 && right < n && s[left] == s[right])
-                {
-                    count++;
-                    left--;
-                    right++;
-                }
-                left = i - 1;
-                right = i;
+                    count++, left--, right++;
+                
+                left = i - 1, right = i;
                 while (left >= 0 && right < n && s[left] == s[right])
-                {
-                    count++;
-                    left--;
-                    right++;
-                }
+                    count++, left--, right++;
             }
             return count;
         }
