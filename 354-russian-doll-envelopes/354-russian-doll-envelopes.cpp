b2eb57b49d21749877c8967015a056ffff8414e7
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-05-25 12:29:48 +0530

    Time: 386 ms (84.46%), Space: 82.8 MB (30.29%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,18 +1,24 @@
 class Solution {
-    static bool cmp(const vector<int> &A, const vector<int> &B){
-        return A[0]==B[0]? A[1]>B[1]:A[0]<B[0];
+    static bool cmp(const pair<int, int> &A, const pair<int, int> &B){
+        return A.first==B.first? A.second>B.second:A.first<B.first;
     }
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
+        vector<pair<int, int> > temp, seq;
         int n = envelopes.size();
+        for (int i = 0; i < n; i++) {
+            pair<int, int> p;
+            p.first = envelopes[i][0];
+            p.second = envelopes[i][1];
+            temp.push_back(p);
+        }
+        sort(temp.begin(), temp.end(), cmp);
         vector<int> res;
-        sort(envelopes.begin(),envelopes.end(), cmp);
-        for(int i=0;i<n;i++){
-            auto it = lower_bound(res.begin(),res.end(),envelopes[i][1]);
-            if(it==res.end()) 
-                res.push_back(envelopes[i][1]);
-            else    
-                *it = envelopes[i][1];
+        for(auto& pr: temp)
+        {
+            auto itr = lower_bound(res.begin(), res.end(), pr.second);
+            if(itr == res.end()) res.push_back(pr.second);
+            else if(*itr > pr.second) *itr = pr.second;
         }
         return res.size();
     }
