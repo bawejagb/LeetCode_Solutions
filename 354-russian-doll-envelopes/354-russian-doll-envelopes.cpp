50aeb0769a92f6ed9765903bc735c92be7ffd509
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-05-25 12:32:35 +0530

    Time: 383 ms (85.66%), Space: 82.8 MB (30.29%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -6,15 +6,18 @@ public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         vector<pair<int, int> > temp, seq;
         int n = envelopes.size();
+        pair<int, int> p;
         for (int i = 0; i < n; i++) {
-            temp.push_back({envelopes[i][0],envelopes[i][1]});
+            p.first = envelopes[i][0];
+            p.second = envelopes[i][1];
+            temp.push_back(p);
         }
         sort(temp.begin(), temp.end(), cmp);
         vector<int> res;
         for(auto& pr: temp)
         {
             auto itr = lower_bound(res.begin(), res.end(), pr.second);
-            if(itr == res.end()) res.emplace_back(pr.second);
+            if(itr == res.end()) res.push_back(pr.second);
             else if(*itr > pr.second) *itr = pr.second;
         }
         return res.size();
