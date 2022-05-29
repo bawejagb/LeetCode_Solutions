995ee4217badd5cecdd93a4ccfdd2e68c3d32d4c
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-05-29 17:43:06 +0530

    Time: 43 ms (94.22%), Space: 15.4 MB (94.99%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
-        int res=0,n = words.size();
-        int arr[1000];
+        int res = 0, n = words.size();
+        int arr[n];
         for(int i=0;i<n;i++){
             int cur=0;
             for(int j=0;j<words[i].size();j++)
