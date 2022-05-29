efce0f695a2bbace9c0f27deeb6f4cc28ccf9afa
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-05-29 17:42:41 +0530

    Time: 49 ms (85.86%), Space: 15.5 MB (94.99%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
-        int dp[1001][26]{};
-        int res=0, n = words.size();
+        int res=0,n = words.size();
+        int arr[1000];
         for(int i=0;i<n;i++){
-            for(char c : words[i])
-                dp[i][c-'a']++;
+            int cur=0;
+            for(int j=0;j<words[i].size();j++)
+                cur = cur | 1<<(words[i][j]-'a');
+            arr[i] = cur;
         }
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                bool fg=0;
-                for(int k=0;k<26;k++){
-                    if(dp[i][k]&&dp[j][k]){
-                        fg=1;
-                        break;
-                    }
-                }
-                if(!fg){
-                    int sz = words[i].size()*words[j].size();
-                    res = max(res,sz);
+                if((arr[i]&arr[j])==0){
+                    int val = words[i].size()*words[j].size();
+                    res = max(res,val);
                 }
             }
         }
