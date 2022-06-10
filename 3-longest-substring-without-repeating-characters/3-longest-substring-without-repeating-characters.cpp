efe2a673f88e55a39869f1c5261f709c498ba776
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-06-10 07:39:43 +0530

    Time: 5 ms (93.48%), Space: 7 MB (88.52%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -8,9 +8,8 @@ class Solution
             for (int i = 0; i < s.size(); i++)
             {
                 if (track[s[i]] > left)
-                {
                     left = track[s[i]];
-                }
+                
                 track[s[i]] = i + 1;
                 res = max(res, i - left + 1);
             }
