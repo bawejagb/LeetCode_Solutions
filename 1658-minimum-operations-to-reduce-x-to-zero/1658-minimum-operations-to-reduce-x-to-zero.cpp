6b31e4a127d20db881cedcbe1ef97fd6ec5228e7
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-06-11 16:07:56 +0530

    Time: 309 ms (51.55%), Space: 98.4 MB (90.95%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
-        int sum,val,res;
-        sum=val=res=0;
-        unordered_map<int,int> hmap;
-        for(int i=0;i<n;i++){
-            sum += nums[i];
-            hmap[sum] = i;
+      int sum = 0;
+      for(int i = 0;i<n;i++){
+        sum += nums[i];
+      }
+      if(sum < x)return -1;
+      if(sum == x)return n;
+      int current_sum = 0;
+      int target = sum - x;
+      int maxi = 0;
+      int start = 0;
+      for(int i = 0;i<nums.size();i++){
+        current_sum += nums[i];
+        while(current_sum > target){
+          current_sum -= nums[start++];
         }
-        hmap[0] = 0;
-        sum -= x;
-        for(int i=0;i<n;i++){
-            val += nums[i];
-            if(hmap.find(val-sum)!=hmap.end()){
-                if(val==sum)
-                    res = max(res,i-hmap[val-sum]+1);
-                else
-                    res = max(res, i-hmap[val-sum]);
-            }
+        if(current_sum == target){
+          maxi = max(maxi,i-start+1);
         }
-        return res==0?(sum==0?n:-1):n-res;
+      }return maxi == 0 ? -1 : n-maxi;
     }
 };
