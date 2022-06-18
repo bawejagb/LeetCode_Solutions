3d7afd0a8b2b7959f29f0fe1e7368a12eb534056
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-06-18 12:56:12 +0530

    Time: 495 ms (79.48%), Space: 104.9 MB (95.90%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,25 +1,50 @@
 class WordFilter {
-    unordered_map<string,int> hmap;
+  struct Node {
+    std::vector<int> indices_;
+    std::array<Node*, 26>* children_{nullptr};
+  } prefix_, suffix_;
+  
+  template<typename It>
+  void add(It begin, It end, int idx, Node* target) {
+    for (; begin != end; ++begin) {
+      if (!target->children_) target->children_ = new std::array<Node*, 26>{};
+      Node* next = (*target->children_)[*begin - 'a'];
+      if (!next) {
+        next = new Node{};
+        (*target->children_)[*begin - 'a'] = next;
+      }
+      target = next;
+      target->indices_.push_back(idx);
+    }
+  }
+  
+  template<typename It>
+  const std::vector<int>* find(It begin, It end, const Node* target) const {
+    for (; begin != end; ++begin) {
+      if (!target->children_) return nullptr;
+      target = (*target->children_)[*begin - 'a'];
+      if (!target) return nullptr;
+    }
+    return &target->indices_;
+  }
+  
 public:
-    WordFilter(vector<string>& words) {
-        for(int i=0;i<words.size();i++){
-            for(int j=0;j<words[i].size();j++){
-                for(int k=0;k<words[i].size();k++){
-                string s = words[i].substr(0,j+1)+'#'+words[i].substr(k,words[i].size());
-                hmap[s] = i;
-                }
-            }
-        }
+  WordFilter(const vector<string>& words) {
+    for (int idx = 0; idx < words.size(); ++idx) {
+      add(words[idx].cbegin(), words[idx].cend(), idx, &prefix_);
+      add(words[idx].crbegin(), words[idx].crend(), idx, &suffix_);
     }
+  }
     
-    int f(string prefix, string suffix) {
-        string s = prefix+'#'+suffix;
-        return hmap.find(s)==hmap.end()?-1:hmap[s];
+  int f(const string& prefix, const string& suffix) {
+    const auto* pre = find(prefix.cbegin(), prefix.cend(), &prefix_);
+    if (!pre) return -1;
+    const auto* suff = find(suffix.crbegin(), suffix.crend(), &suffix_);
+    if (!suff) return -1;
+    if (pre->size() > suff->size()) std::swap(pre, suff);
+    for (auto it = pre->crbegin(); it != pre->crend(); ++it) {
+      if (std::binary_search(suff->cbegin(), suff->cend(), *it)) return *it;
     }
+    return -1;
+  }
 };
-
-/**
- * Your WordFilter object will be instantiated and called as such:
- * WordFilter* obj = new WordFilter(words);
- * int param_1 = obj->f(prefix,suffix);
- */
