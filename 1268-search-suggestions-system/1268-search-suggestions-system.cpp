921eb28b1e6a011d7cba88440c87e70f75403a19
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-06-19 11:47:24 +0530

    Time: 59 ms (84.15%), Space: 23.4 MB (96.74%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,57 +1,19 @@
-class Trie{
-    struct Node{
-        bool isWord = false;
-        vector<Node*> children{vector<Node*>(26,NULL)};
-    } *Root,*curr;
-    void dfs(Node* curr, string &word, vector<string> &result){
-        if(result.size()==3) return;
-        if(curr->isWord)
-            result.push_back(word);
-        for(char c = 'a'; c<='z';c++){
-            if(curr->children[c-'a']){
-                word +=c;
-                dfs(curr->children[c-'a'],word,result);
-                word.pop_back();
-            }
-        }
-    }
-public:
-    Trie(){
-        Root = new Node();
-    }
-    void insert(string &s){
-        curr = Root;
-        for(char c: s){
-            if(!curr->children[c-'a'])
-                curr->children[c-'a'] = new Node();
-            curr = curr->children[c-'a'];
-        }
-        curr->isWord = true;
-    }
-    vector<string> getWordStartingWith(string &prefix){
-        curr = Root;
-        vector<string> result;
-        for(char &c : prefix){
-            if(!curr->children[c-'a'])
-                return result;
-            curr = curr->children[c-'a'];
-        }
-        dfs(curr, prefix, result);
-        return result;
-    }
-};
 class Solution {
-    
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        Trie trie=Trie();
-        vector<vector<string>> res;
-        for(string &w: products) trie.insert(w);
-        string prefix;
-        for(char &c: searchWord){
-            prefix += c;
-            res.push_back(trie.getWordStartingWith(prefix));
+   
+     vector<vector<string>> ans;
+        sort(products.begin(), products.end());
+        string key;
+        for(auto c: searchWord){
+            key += c;
+            auto l=lower_bound(products.begin(), products.end(), key);
+            auto r=upper_bound(products.begin(), products.end(), key+='~');
+            key.pop_back();
+            if(l == r) break;
+            ans.emplace_back(l, min(l+3, r));            
         }
-        return res;
+        while(ans.size() != searchWord.size()) ans.push_back({});
+        return ans; 
     }
 };
