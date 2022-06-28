833e2dd433e1e15c4c92d6ea5e6a194612e680ee
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-06-28 08:41:14 +0530

    Time: 35 ms (43.26%), Space: 7.8 MB (51.14%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -4,15 +4,12 @@ public:
     string minWindow(string s, string t) {
         int n=s.length();
         int m=t.length();
-        
-        int i,j;
+        int i,j,start,end;
         map<char,int> mp;
         for(auto it: t){
             mp[it]++;
         }
-        
-        int curr=0,ans=n+1,start=-1,end=-1;
-        
+        int curr=0,ans=n+1;
         i=0,j=0;
         while (j<n){
             if (mp[s[j]]>0){
