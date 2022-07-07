2a145ac38df7ca8d925c5540783321d9abd6241a
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-07-07 09:15:28 +0530

    Time: 7 ms (57.29%), Space: 7 MB (54.34%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int m = s1.size() , n = s2.size();
+        if(m + n != s3.size() ) return false;
+        vector<vector<int>> dp(m+1, vector<int> (n+1 , 0));
+        dp[0][0] = 1;
+        for(int i=1; i<= m; i++) dp[i][0] = (s1[i-1] == s3[i-1] ) && (dp[i-1][0]) ;
+        for(int i=1; i<= n; i++) dp[0][i] = (s2[i-1] == s3[i-1] ) && (dp[0][i-1]) ;
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                dp[i][j] = (s3[i+j-1] == s1[i-1] ? dp[i-1][j] : 0 ) || 
+                    (s3[i+j-1] == s2[j-1] ? dp[i][j-1] : 0)  ;
+            }
+        }
+
+        return dp[m][n];
+    }   
+};
