768ef99c7c49dd207e5d6b67b9960f3db629afb4
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-07-17 11:36:54 +0530

    Time: 66 ms (49.36%), Space: 16.3 MB (31.41%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    int m = 1e9+7;
+public:
+    int kInversePairs(int n, int k) {
+        if(k==0)
+            return 1;
+        int i,j; 
+        vector<vector<int>> dp(n+1,vector<int>(k+1,0));
+        dp[0][0]=1;
+        for(i=1;i<=n;i++){
+            dp[i][0]=1;
+            for(j=1;j<=k;j++){
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j])%m;
+                if(j>=i)
+                    dp[i][j] = (dp[i][j] - dp[i-1][j-i] + m)%m;
+            }
+        }
+        return dp[n][k];
+    }
+};
