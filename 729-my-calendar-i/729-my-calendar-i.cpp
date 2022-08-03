c1a3758d61e44f5761f15bd732cc7477b6a9dafd
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-08-03 17:54:13 +0530

    Time: 97 ms (93.81%), Space: 39.3 MB (23.89%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,16 +1,34 @@
 class MyCalendar {
 public:
-    vector<pair<int,int>> v;
-    MyCalendar() { 
+    set<pair<int,int>> hset;
+    MyCalendar() {
+
     }
     
     bool book(int start, int end) {
-        for(auto it: v){
-            if(it.first<end && start<it.second)
-                return false;                                                                // 
+        auto it = hset.lower_bound({start,end});
+        bool ans;
+        if(it==hset.end()){
+            if(hset.size()){
+            auto it1=it;
+            it1--;
+            if(start>=(*it1).second) ans=true;
+            else ans =false;
+            }else
+            ans=true;
         }
-        v.push_back({start,end});
-        return true;
+        else if(it==hset.begin()){
+            if(end<=(*it).first) ans= true;
+            else ans=false;
+        }else{
+            auto it1=it;
+            it1--;
+            if(end<=(*it).first&&start>=(*it1).second) ans=true;
+            else ans =false;
+        }
+        if(ans)
+        hset.insert({start,end});
+        return ans;
     }
 };
 
