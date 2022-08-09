b699109464eb056d9eaad9626bd0f6553084aaa8
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-08-09 22:39:45 +0530

    Time: 37 ms (98.46%), Space: 9.2 MB (81.85%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,30 +1,25 @@
 class Solution {
-    int MOD= 1e9+7;
 public:
     int numFactoredBinaryTrees(vector<int>& arr) {
-        int res=0;
-        if(arr.size()==1)
-            return 1;
-        unordered_map<int,int> mp;
-        vector<int> dp(arr.size(),1);
-        sort(arr.begin(),arr.end());
-        mp[arr[0]]=1;
-        for(int i=1;i<arr.size();i++){
-            mp[arr[i]]=i+1;
-            for(int j=0;j<i;j++){
-                if(arr[i]%arr[j]==0){
-                    int k= arr[i]/arr[j];
-                    if(mp[k]){
-                         dp[i]+= ((1LL*dp[mp[arr[j]]-1])*(1LL*dp[mp[k]-1]))%MOD;
-                         dp[i]= dp[i]%MOD;
-                    }
-                }
+        sort(arr.begin(), arr.end());
+        long Ans = 0;
+        long mx = 1e9 + 7;
+        unordered_map<int, long> SubTree;
+        
+        for (int num: arr) {
+            long ways = 1;
+            double lim = sqrt(num);
+            for (int i=0, childA = arr[0]; childA<=lim; childA = arr[++i]) {
+                if (num % childA != 0)
+                    continue;
+                int childB = num/childA;
+                if (SubTree.find(childB) != SubTree.end())
+                    ways = (ways + SubTree[childA]*SubTree[childB]*(childA == childB? 1:2) % mx);
             }
+            SubTree[num] = ways;
+            Ans = (Ans + ways) % mx;
         }
-        for(auto it: dp){
-            res+= it;
-            res= res%MOD;
-        }
-        return res;
+        
+        return (int)Ans;
     }
 };
