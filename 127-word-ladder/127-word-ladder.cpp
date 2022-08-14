64d7b83938b0e277a5809d3f5e3c4204e9ae2a01
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-08-14 18:43:19 +0530

    Time: 216 ms (63.20%), Space: 31.7 MB (17.00%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        map<string,vector<string>> hmap;
+        int n = wordList.size();
+        int sz = beginWord.size();
+        for(int i = 0; i < n; i++){
+            for(int j=0;j<sz;j++){
+                string s = wordList[i].substr(0,j)+"*"+wordList[i].substr(j+1,sz-j-1);
+                hmap[s].push_back(wordList[i]);
+            }
+        }
+        for(int i=0;i<sz;i++){
+            string s = beginWord.substr(0,i)+"*"+beginWord.substr(i+1,sz-i-1);
+            hmap[s].push_back(beginWord);
+        }
+        queue<string> qt;
+        qt.push(beginWord);
+        set<string> st;
+        st.insert(beginWord);
+        int res=1;
+        while(!qt.empty()){
+            int len = qt.size();
+            while(len--){
+                string s = qt.front();
+                qt.pop();
+                for(int i=0;i<sz;i++){
+                    string t = s.substr(0,i)+"*"+s.substr(i+1,sz-i-1);
+                    for(string p : hmap[t]){
+                        if(st.count(p)==0){
+                            st.insert(p);
+                            qt.push(p);
+                        }
+                    }
+                }
+            }
+            res++;
+            if(st.count(endWord)) return res;
+        }
+        return 0;
+    }
+};
