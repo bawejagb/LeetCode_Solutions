4a4cbd6a30d83cc3d86b6303605a9c6e7eebf0da
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-08-29 23:44:52 +0530

    Time: 55 ms (53.21%), Space: 12.3 MB (83.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,30 +1,10 @@
 class Solution {
-    void bfs(vector<vector<char>>& grid, int a, int b){
+    void dfs(vector<vector<char>>& grid, int a, int b){
         grid[a][b]='2';
-        queue<pair<int,int>> qt;
-        qt.push({a,b});
-        while(!qt.empty()){
-            auto pr = qt.front();
-            qt.pop();
-            a = pr.first;
-            b = pr.second;
-            if(a>0&& grid[a-1][b]=='1'){
-                grid[a-1][b]='2';
-                qt.push({a-1,b});
-            }
-            if(b>0&& grid[a][b-1]=='1'){
-                grid[a][b-1]='2';
-                qt.push({a,b-1});
-            }
-            if(a<grid.size()-1&&grid[a+1][b]=='1'){
-                grid[a+1][b]='2';
-                qt.push({a+1,b});
-            }
-            if(b<grid[0].size()-1&&grid[a][b+1]=='1') {
-                grid[a][b+1]='2';
-                qt.push({a,b+1});
-            }
-        }
+        if(a>0&& grid[a-1][b]=='1') dfs(grid, a-1,b);
+        if(b>0&& grid[a][b-1]=='1') dfs(grid, a,b-1);
+        if(a<grid.size()-1&&grid[a+1][b]=='1') dfs(grid, a+1,b);
+        if(b<grid[0].size()-1&&grid[a][b+1]=='1') dfs(grid, a,b+1);
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
@@ -34,7 +14,7 @@ public:
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    bfs(grid, i, j);
+                    dfs(grid, i, j);
                 }
             }
         }
