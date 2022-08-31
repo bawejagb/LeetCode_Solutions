9222f362da575cd00519813771aa2bebe9b4d8c6
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-08-31 10:33:40 +0530

    Time: 88 ms (35.13%), Space: 16.8 MB (99.07%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,16 +1,16 @@
 class Solution {
-    void dfs(vector<vector<int>>& heights,bool flag[201][201], int row, int col){
-        flag[row][col]=true;
-        int n = heights.size();
-        int m = heights[0].size();
-        if(row>0 && !flag[row-1][col] && heights[row-1][col]>=heights[row][col])
-            dfs(heights, flag, row-1, col);
-        if(row<n-1 && !flag[row+1][col] && heights[row+1][col]>=heights[row][col])
-            dfs(heights, flag, row+1, col);
-        if(col>0 && !flag[row][col-1] && heights[row][col-1]>=heights[row][col])
-            dfs(heights, flag, row, col-1);
-        if(col<m-1 && !flag[row][col+1] && heights[row][col+1]>=heights[row][col])
-            dfs(heights, flag, row, col+1);
+    void dfs(vector<vector<int>>& heights,bool flag[201][201], int prevheight, int i, int j){
+        if(i < 0 || j < 0 || i >= heights.size() || j >= heights[0].size() || flag[i][j] || prevheight > heights[i][j]){
+            return;
+        }
+        else{
+            flag[i][j] = 1;
+            dfs(heights, flag, heights[i][j], i+1, j);
+            dfs(heights, flag, heights[i][j], i-1, j);
+            dfs(heights, flag, heights[i][j], i, j+1);
+            dfs(heights, flag, heights[i][j], i, j-1);
+            return;
+        }
     }
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
@@ -20,14 +20,14 @@ public:
         bool pacaficFlag[201][201]{}, atlanticFlag[201][201]{};
         //Pacific Ocean Visit
         for(int i=0;i<n;i++)
-            dfs(heights, pacaficFlag, i,0);
+            dfs(heights, pacaficFlag, 0,i,0);
         for(int i=0;i<m;i++)
-            dfs(heights, pacaficFlag, 0,i);
+            dfs(heights, pacaficFlag, 0,0,i);
         //Atlantic Ocean Visit
         for(int i=0;i<n;i++)
-            dfs(heights, atlanticFlag, i,m-1);
+            dfs(heights, atlanticFlag, 0,i,m-1);
         for(int i=0;i<m;i++)
-            dfs(heights, atlanticFlag, n-1,i);
+            dfs(heights, atlanticFlag, 0,n-1,i);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(pacaficFlag[i][j]&&atlanticFlag[i][j])
