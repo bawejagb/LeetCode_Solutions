c3092f6b29383c4f907d832f2318c7ca256be54f
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-09-08 20:18:58 +0530

    Time: 23 ms (98.10%), Space: 7.5 MB (83.17%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        int cnt,mx = 2;
-        if(n<=2) return n;
-        for(int i=0;i<n-1;i++){
-            for(int j = i+1;j<n;j++){
-                cnt=2;
-                for(int k = 0; k < n; k++){
-                    if(k != i && k != j){
-                        if((points[j][1]-points[i][1])*(points[k][0]-points[i][0])==(points[j][0]-points[i][0])*(points[k][1]-points[i][1]))
-                            cnt++;
-                    }
+        vector<double> s;
+        int ans = 0, c = 0, f = 0, u = 0;
+        int n = points.size(); 
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j < n; j++){
+                double y = points[i][1] - points[j][1];
+                double x = points[i][0] - points[j][0];
+                
+                if(x == 0 || y == 0){
+                    if(x == 0)
+                        f++;
+                    
+                    if(y == 0)
+                        u++;
                 }
-                mx = max(cnt,mx);
+                else
+                    s.push_back(y / x);
             }
+            sort(s.begin(),s.end());
+            for(int j = 1; j < s.size(); j++){
+                if(s[j] == s[j - 1])
+                    c++;
+                else
+                {
+                    if(c + 1 > ans)
+                        ans = c + 1;
+                    
+                    c = 0;
+                }
+            }
+            if(c + 1 > ans)
+                ans = c + 1;
+            c = 0;
+            if(f > ans)
+                ans = f;
+            if(u > ans)
+                ans = u;
+            s.clear();
+            f = 0;
+            u = 0;
         }
-        return mx;
+        return ans + 1;
     }
 };
