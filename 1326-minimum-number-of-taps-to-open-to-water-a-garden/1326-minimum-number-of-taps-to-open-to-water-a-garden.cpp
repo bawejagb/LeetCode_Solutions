6d0b96f6bdfc751e88a824f9c99643582d0fca3e
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-09-09 22:16:52 +0530

    Time: 108 ms (17.28%), Space: 22.8 MB (6.71%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<int> dp(n+1,n+1);
-        dp[0] = 0;
-        for(int i=0;i<=n;i++){
-            int left = max(0,i-ranges[i]);
-            int right = min(n, i+ranges[i]);
-            for(int j = left; j <= right; j++){
-                dp[j] = min(dp[j], dp[left]+1);
+        vector<vector<int>> area;
+        for(int i = 0; i <=n; i++)
+            area.push_back({i - ranges[i], i + ranges[i]});
+        sort(area.begin(), area.end());
+        int i = 0, start = 0, end = 0, ans = 0;
+        while(start < n) {
+            while(i < area.size() && area[i][0] <= start) {
+                end = max(end, area[i][1]);
+                i++;
             }
+            if(start == end) return -1;
+            start = end;
+            ans++;
         }
-        return dp[n]==n+1? -1 : dp[n];
+        return ans;
     }
 };
