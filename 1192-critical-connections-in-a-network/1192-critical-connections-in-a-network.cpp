da55b2e7951317f625d8f79f8abef5056f51d1f5
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-09-12 11:45:13 +0530

    Time: 1499 ms (9.19%), Space: 193 MB (26.28%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    vector<int> dis,low;
+    vector<int> low, dis;
     vector<vector<int>> bridge, adj;
     int time;
     void dfs(int node, int prev){
@@ -7,26 +7,26 @@ class Solution {
         low[node] = dis[node] = time;
         for(auto x : adj[node]){
             if(x==prev) continue;
-            else if(dis[x]!=-1){
-               low[node] = min(low[node],dis[x]);
+            if(dis[x]!=-1){
+                low[node] = min(low[node], dis[x]);
                 continue;
             }
-            dfs(x,node);
-            low[node] = min(low[node],low[x]);
-            if(low[x] > dis[node])
-                bridge.push_back({node,x});
+            dfs(x, node);
+            low[node] = min(low[node], low[x]);
+            if(low[x]>dis[node])
+                bridge.push_back({node, x});
         }
-        
     }
+    
 public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        time = 0;
         dis.resize(n,-1);
         low.resize(n,-1);
         adj.resize(n);
-        time=0;
-        for(auto x : connections){
-            adj[x[0]].push_back(x[1]);
-            adj[x[1]].push_back(x[0]);
+        for(auto c : connections){
+            adj[c[0]].push_back(c[1]);
+            adj[c[1]].push_back(c[0]);
         }
         for(int i=0;i<n;i++){
             if(dis[i]==-1)
