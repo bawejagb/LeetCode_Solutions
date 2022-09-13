34d05f19f6dde8e645eb3242acd952520d800239
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-09-13 11:53:36 +0530

    Time: 3 ms (98.04%), Space: 7.4 MB (97.21%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -1,63 +1,22 @@
 class Solution {
-    unordered_set<string> ans;
-    bool isValid(string & st){
-        int n = st.size();
-        int cnt = 0;
-        for(int i = 0; i< n; i++){
-            if(st[i]=='(') cnt++;
-            else if(st[i]==')') cnt--;
-            if(cnt < 0) return false;
+    void dfs(int p, int lastRm, string dir, string s, vector<string>& res) {
+        for(int i=p, count=0;i<s.size();i++) {
+            if(s[i]==dir[0]) count++;
+            else if(s[i]==dir[1]) count--;
+            if(count>=0) continue;
+            for(int j=lastRm;j<=i;j++)
+                if(s[j]==dir[1] && (j==lastRm || s[j-1]!=dir[1]))
+                    dfs(i,j,dir,s.substr(0,j)+s.substr(j+1),res);
+            return;        
         }
-        return cnt == 0;
-    }
-    void dfs(string s, int idx, string st, int inValidCount, int totalInValid){
-        if(idx==s.size()){
-            if(isValid(st))
-                ans.insert(st);
-            return;
-        }
-        if(totalInValid-inValidCount==s.size()-idx){
-            dfs(s,s.size(),st,totalInValid, totalInValid);
-            return;
-        }
-        if(inValidCount == totalInValid){
-            string temp = st+s.substr(idx);
-            dfs(s,s.size(),temp, inValidCount, totalInValid);
-            return;
-        }
-        st += s[idx];
-        dfs(s,idx+1, st, inValidCount, totalInValid);
-        st.pop_back();
-        if(s[idx]!='(' and s[idx]!=')') return;
-        dfs(s,idx+1, st, inValidCount+1, totalInValid);
+        reverse(s.begin(),s.end());
+        if(dir[0]=='(') dfs(0,0,")(",s,res);
+        else res.push_back(s);
     }
 public:
     vector<string> removeInvalidParentheses(string s) {
-        int n = s.size(), cnt = 0;
-        int totalInValid = 0;
-        for(int i=0;i<n;i++){
-            if(s[i]=='('){
-                if(cnt<0) cnt = 1;
-                else cnt++;
-            }
-            else if(s[i]==')') cnt--;
-            if(cnt<0 and s[i]==')') totalInValid++;
-        }
-        cnt = 0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]==')'){
-                if(cnt<0) cnt = 1;
-                else cnt++;
-            }
-            else if(s[i]=='(') cnt--;
-            if(cnt<0 and s[i] =='(') totalInValid++;
-        }
-        string st;
-        dfs(s,0,st,0,totalInValid);
         vector<string> res;
-        for(auto &i : ans){
-            res.push_back(i);
-        }
+        dfs(0,0,"()",s,res);
         return res;
     }
 };
