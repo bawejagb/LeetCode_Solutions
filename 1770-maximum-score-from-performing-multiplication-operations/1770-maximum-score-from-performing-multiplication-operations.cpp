0edbfcc40db847b0792893f14445f073dc344cc2
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-09-16 11:46:32 +0530

    Time: 238 ms (99.73%), Space: 54.7 MB (98.40%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,15 +1,16 @@
 class Solution {
-    int helper(vector<int>& nums, vector<int>& mul, int idx, int left, vector<vector<int>>&dp){
-        if(idx==mul.size()) return 0;
-        if(dp[left][idx]!=INT_MAX) return dp[left][idx];
-        int l = mul[idx]*nums[left]+helper(nums,mul,idx+1,left+1,dp);
-        int r = mul[idx]*nums[nums.size()-1-idx+left]+helper(nums,mul,idx+1,left,dp);
-        return dp[left][idx] = max(l,r);
-    }
+    
 public:
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
         int m = multipliers.size();
-        vector<vector<int>> dp(m+1, vector<int>(m+1,INT_MAX));
-        return helper(nums, multipliers, 0, 0,dp);
+        int n = nums.size();
+        int dp[1001][1001]{};
+        for(int i = m-1; i>=0; i--){
+            for(int left = i; left>=0; left--){
+                dp[i][left] = max(nums[left]*multipliers[i]+dp[i+1][left+1],
+                                 nums[n-1-i+left]*multipliers[i]+dp[i+1][left]);
+            }
+        }
+        return dp[0][0];
     }
 };
