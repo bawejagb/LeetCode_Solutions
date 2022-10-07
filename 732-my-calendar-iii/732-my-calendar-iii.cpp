2dfc3f24a4ebdb6f01cabcafd394f6878a5f247c
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-10-07 15:23:56 +0530

    Time: 1411 ms (5.03%), Space: 308.3 MB (5.03%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -1,19 +1,26 @@
 class MyCalendarThree {
-    map<int,int> hmap;
+    unordered_map<int,int> vals, lazy;
+    void update(int start, int end, int left, int right, int idx){
+        if(start>right || end < left) return;
+        if(start <= left and end >= right){
+            vals[idx]++;
+            lazy[idx]++;
+        }
+        else{
+            int mid= (left+right)/2;
+            update(start, end, left, mid, idx*2);
+            update(start, end, mid+1, right, idx*2+1);
+            vals[idx] = lazy[idx]+max(vals[idx*2], vals[idx*2+1]);
+        }
+    }
 public:
     MyCalendarThree() {
         
     }
     
     int book(int start, int end) {
-        hmap[start]++;
-        hmap[end]--;
-        int res=0,cur=0;
-        for(auto &[a,b] : hmap){
-            cur += b;
-            res = max(res,cur);
-        }
-        return res;
+        update(start, end-1, 1, 1e9, 1);
+        return vals[1];
     }
 };
 
