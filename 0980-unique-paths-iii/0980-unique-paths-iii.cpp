171215cee28c97f60ce73ecd97c3d47edb40bb3c
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-10-12 10:51:29 +0530

    Time: 3 ms (83.28%), Space: 7.1 MB (28.16%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    int path=0;
+    int dx[4] = {1,-1,0,0};
+    int dy[4] = {0,0,1,-1};
+    void backtrack(vector<vector<int>>& grid, int a, int b, int cnt, int m, int n){
+        for(int i=0;i<4;i++){
+            int nx = dx[i]+a;
+            int ny = dy[i]+b;
+            if(nx<0||ny<0||nx==m||ny==n||grid[nx][ny]==-1||grid[nx][ny]==3) continue;
+            if(cnt==0 && grid[nx][ny]==2) path++;
+            else if(grid[nx][ny]==0){
+                grid[nx][ny]=3;
+                backtrack(grid,nx,ny,cnt-1,m,n);
+                grid[nx][ny]=0;
+            }
+        }
+    }
+public:
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        int empty=0;
+        int m = grid.size();
+        int n = grid[0].size();
+        int a,b;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0) empty++;
+                else if(grid[i][j]==1) a=i,b=j;
+            }
+        }
+        backtrack(grid,a,b,empty,m,n);
+        return path;
+    }
+};
