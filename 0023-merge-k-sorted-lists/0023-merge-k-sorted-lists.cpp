5c570e40be709544af88bca730d5c46d80f42d8a
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-10-14 10:29:22 +0530

    Time: 48 ms (56.11%), Space: 13.5 MB (48.45%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,25 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
 class Solution {
-    typedef pair<int, ListNode*> pii;
+    ListNode* mergeTwoList(ListNode* l1, ListNode* l2) {
+        ListNode* result = nullptr;
+        if (!l1) return l2;
+        if (!l2) return l1;
+        if (l1->val < l2->val) {
+            result = l1;
+            l1 = l1->next;
+        } else {
+            result = l2;
+            l2 = l2->next;
+        }
+        ListNode* node = result;
+        while(l1 != nullptr || l2 != nullptr) {
+            if (l1 == nullptr) {
+                node->next = l2;
+                break;
+            } else if (l2 == nullptr) {
+                node->next = l1;
+                break;
+            } else if (l1->val < l2->val) {
+                node->next = l1;
+                l1 = l1->next;
+            } else {
+                node->next = l2;
+                l2 = l2->next;
+            }
+            node = node->next;
+        }
+        return result;
+    }
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<pii, vector<pii>, greater<pii>> qt;
-        for(int i = 0; i< lists.size();i++)
-            if(lists[i])
-                qt.push({lists[i]->val,lists[i]});
-        ListNode* head = new ListNode(0);
-        ListNode* temp = head;
-        while(!qt.empty()){
-            auto node = qt.top();
-            qt.pop();
-            temp->next = new ListNode(node.first);
-            temp = temp->next;
-            node.second = node.second->next;
-            if(node.second)
-                qt.push({node.second->val, node.second});
+        const int n = lists.size();
+        if (n == 0) return nullptr;
+        std::queue<ListNode*> nodes;
+        for (ListNode* node : lists) nodes.push(node);
+        while(nodes.size() > 1) {
+            ListNode* l1 = nodes.front();
+            nodes.pop();
+            ListNode* l2 = nodes.front();
+            nodes.pop();
+            ListNode* m = mergeTwoList(l1, l2);
+            nodes.push(m);
         }
-        return head->next;
+        return nodes.front(); 
     }
 };
