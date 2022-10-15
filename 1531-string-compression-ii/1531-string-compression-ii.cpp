c8a3abae415b86dd3dc8c907eadb9b356e13f416
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-10-15 22:40:14 +0530

    Time: 25 ms (97.73%), Space: 6.3 MB (90.15%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.cpp
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -1,27 +1,26 @@
-int dp[101][27][101][101];
+int dp[101][101];
 class Solution {
-    
     string st;
-    int findLength(int idx, int prev, int cnt, int k){
-        if(k<0) return INT_MAX;
-        if(idx>=st.size()) return 0;
-        if(dp[idx][prev][cnt][k]!=-1) return dp[idx][prev][cnt][k];
-        int miss = findLength(idx+1, prev, cnt, k-1);
-        int notMiss = 0;
-        if(prev == st[idx]-'a'){
-            if(cnt==1 || cnt==9 || cnt==99)
-                notMiss++;
-            notMiss += findLength(idx+1, prev, cnt+1, k);
+    int findLength(int idx, int K){
+        int k = K;
+        if(st.size()-idx<=k) return 0;
+        if(dp[idx][k]!=-1) return dp[idx][k];
+        int res = k ? findLength(idx+1, k-1): 1e5;
+        int cnt = 1;
+        for(int i=idx+1;i<=st.size();++i){
+            int notMiss = 1+findLength(i, k);
+            notMiss += (cnt>=100?3:(cnt>=10?2:(cnt>1?1:0)));
+            res = min(res, notMiss);
+            if(i==st.size()) break;
+            if(st[i]==st[idx]) cnt++;
+            else if(--k<0) break;
         }
-        else{
-            notMiss = 1 + findLength(idx+1, st[idx]-'a', 1, k);
-        }
-        return dp[idx][prev][cnt][k] = min(miss,notMiss);
+        return dp[idx][K] = res;
     }
 public:
     int getLengthOfOptimalCompression(string s, int k) {
         st = s;
         memset(dp,-1,sizeof(dp));
-        return findLength(0,26, 0, k);
+        return findLength(0, k);
     }
 };
