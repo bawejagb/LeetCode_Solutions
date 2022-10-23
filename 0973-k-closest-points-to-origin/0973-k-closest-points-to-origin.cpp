4f6720611c58d85c0ae262628ec3e55de9d9ad27
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-10-23 12:00:40 +0530

    Time: 381 ms (74.15%), Space: 58.2 MB (87.51%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int,pair<int,int>>> pq;
         int n = points.size();
+        if(n<=k) return points;
+        vector<int> dis(n),idx(n);
+        vector<vector<int>> res(k);
         for(int i=0;i<n;i++){
-            int dis = points[i][0]*points[i][0]+points[i][1]*points[i][1];
-            pq.push({dis,{points[i][0],points[i][1]}});
+            idx[i] = i;
+            dis[i] = points[i][0]*points[i][0]+points[i][1]*points[i][1];
         }
-        n -= k;
-        while(n--) pq.pop();
-        vector<vector<int>> res;
-        while(k--){
-            res.push_back({pq.top().second.first,pq.top().second.second});
-            pq.pop();
+        auto cmp = [&dis](int a, int b){
+            return dis[a]<dis[b];
+        };
+        nth_element(idx.begin(),idx.begin()+k,idx.end(),cmp);
+        for(int i=0;i<k;i++){
+            res[i] = points[idx[i]];
         }
         return res;
     }
