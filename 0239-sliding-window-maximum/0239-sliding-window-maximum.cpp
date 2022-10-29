0d46eb4abb150415b49ac376de5f0e2e68104e8e
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-10-29 23:27:24 +0530

    Time: 769 ms (22.64%), Space: 128.3 MB (99.83%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>> pq;
-        vector<int> res;
-        for(int i=0;i<k-1;i++)
-            pq.push({nums[i],i});
-        for(int i=k-1;i<nums.size();i++){
-            pq.push({nums[i],i});
-            while(pq.top().second<=i-k)
-                pq.pop();
-            res.push_back(pq.top().first);
+        int n = size(nums);
+        int q[n+k];
+        int front = 0;
+        int back = 0;
+        vector<int> ans(n-k+1);
+        for (int i=0; i<k; ++i){
+            int num = nums[i];
+            while(back-front>0 && num>nums[q[back-1]]){
+                --back;
+            }
+            q[back++] = i;
         }
-        return res;
+        for (int i=k; i<n; ++i){
+            int l = i-k;
+            ans[l] = nums[q[front]];
+            int num = nums[i];
+            while(back-front>0 && num>nums[q[back-1]]){
+                --back;
+            }
+            q[back++] = i;
+            if (q[front] <= l){
+                ++front;
+            }
+        }
+        ans[n-k] = nums[q[front]];
+        return ans;
     }
 };
