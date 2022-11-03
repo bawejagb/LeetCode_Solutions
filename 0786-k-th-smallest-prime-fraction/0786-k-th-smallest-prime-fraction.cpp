330c8ea4e9cfdc26af374b4b657a6147bb2b79be
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-11-03 13:44:55 +0530

    Time: 3 ms (99.67%), Space: 8.3 MB (96.20%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,17 +1,33 @@
 class Solution {
-    static bool cmp(pair<int,int>&a, pair<int,int>&b){
-        return (double)a.first/(double)a.second < (double)b.first/(double)b.second;
-    }
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<pair<int,int>> pVec;
-        for(int i=1;i<n;i++){
-            for(int j = 0; j<i;j++){
-                pVec.push_back({arr[j],arr[i]});
+        double l = 0, r = 1.0;
+        while(l<r){
+            double m = (l+r)/2;
+            double max_f = 0.0;
+            int total=0,p=0,q=0;
+            int j=1;
+            for(int i=0;i<n-1;i++){
+                while(j<n && arr[i] > m*arr[j])
+                    j++;
+                total += (n-j);
+                if(j==n)
+                    break;
+                double f = static_cast<double>(arr[i]) / arr[j];
+                if (f > max_f) {
+                  p = i;
+                  q = j;
+                  max_f = f;
+                }
             }
+            if (total == k)
+                return {arr[p], arr[q]}; 
+            else if (total > k)
+                r = m;
+            else
+                l = m;
         }
-        sort(pVec.begin(),pVec.end(), cmp);
-        return {pVec[k-1].first,pVec[k-1].second};
+        return {};
     }
 };
