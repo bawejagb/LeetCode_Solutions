65ad9500e08e07b7ff13622a1be7fd24dd3a2574
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-11-04 20:39:32 +0530

    Time: 1341 ms (41.71%), Space: 173.1 MB (77.67%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,37 +1,28 @@
 class Solution {
-    vector<int> low, dis;
-    vector<vector<int>> bridge, adj;
-    int time;
-    void dfs(int node, int prev){
-        time++;
-        low[node] = dis[node] = time;
-        for(auto x : adj[node]){
-            if(x==prev) continue;
-            if(dis[x]!=-1){
-                low[node] = min(low[node], dis[x]);
-                continue;
-            }
-            dfs(x, node);
-            low[node] = min(low[node], low[x]);
-            if(low[x]>dis[node])
-                bridge.push_back({node, x});
+    const int NO_RANK = -2;
+    int dfs(vector<int> graph[], int n, int node, int myRank, vector<int> &rank, vector<vector<int>> &res){
+        if(rank[node]!=NO_RANK) return rank[node];
+        rank[node] = myRank;
+        int lowRank = myRank;
+        for(int nbr: graph[node]){
+            if(rank[nbr]==myRank-1 || rank[nbr]==n) continue;
+            int nbrRank = dfs(graph, n, nbr, myRank+1, rank, res);
+            if(nbrRank>myRank) res.push_back({min(nbr, node), max(nbr,node)});
+            lowRank = min(lowRank, nbrRank);
         }
+        rank[node] = n;
+        return lowRank;
     }
-    
 public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        time = 0;
-        dis.resize(n,-1);
-        low.resize(n,-1);
-        adj.resize(n);
-        for(auto c : connections){
-            adj[c[0]].push_back(c[1]);
-            adj[c[1]].push_back(c[0]);
+        vector<int> rank(n,NO_RANK);
+        vector<int> graph[n];
+        vector<vector<int>> res;
+        for(auto pr: connections){
+            graph[pr[0]].push_back(pr[1]);
+            graph[pr[1]].push_back(pr[0]);
         }
-        for(int i=0;i<n;i++){
-            if(dis[i]==-1)
-                dfs(i,-1);
-        }
-        return bridge;
+        dfs(graph, n, 0,0, rank,res);
+        return res;
     }
 };
