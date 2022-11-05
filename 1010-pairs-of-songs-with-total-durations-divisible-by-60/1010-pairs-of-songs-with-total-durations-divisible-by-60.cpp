f9f963b96fd25179d6680de1e24ba4c42046c5d9
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-11-05 16:02:42 +0530

    Time: 61 ms (76.51%), Space: 28.1 MB (67.66%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& time) {
         int n = time.size();
-        for(int i=0;i<n;i++)
-            time[i] %= 60;
-        int res=0;
-        map<int,int> hmap;
-        for(int i= 0;i<n;i++){
-            if(hmap[(60-time[i])%60]>0){
-                res += hmap[(60-time[i])%60];
-            }
+        int cnt = 0;
+        for(int i = 0; i < n; i++)
+            time[i] = time[i]%60;
+        long hmap[60]{};
+        for(int i = 0;  i< n; i++)
             hmap[time[i]]++;
+        for(int i = 0; i <= 30; i++){
+            if(hmap[i] == 0) continue;
+            if(i == 0 || i == 30) cnt += (hmap[i]*(hmap[i]-1)/2);
+            else cnt +=(hmap[i]*hmap[60-i]); 
         }
-        return res;
+        return cnt;
     }
 };
