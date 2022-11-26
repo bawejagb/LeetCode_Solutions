0655599d5031e4c2bc3fc26bf9136e6050f36d86
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-11-26 16:43:58 +0530

    Time: 192 ms (85.99%), Space: 56.7 MB (62.88%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,23 +1,24 @@
 class Solution {
+    int dp[500005];
     static bool cmp(const vector<int>&a, const vector<int>&b){
         if(a[0]==b[0]) return a[1]<b[1];
         return a[0]<b[0];
     }
-    int findProfit(vector<vector<int>>&job, int idx, int n, unordered_map<int,int>& memo){
+    int findProfit(vector<vector<int>>&job, int idx, int n){
         if(idx==n) return 0;
-        if(memo.find(idx)!=memo.end()) return memo[idx];
+        if(dp[idx]!=-1) return dp[idx];
         int temp = idx;
         while(temp<n and job[temp][0]<job[idx][1]) temp++;
         int res;
-        res = findProfit(job,temp,n, memo)+job[idx][2];
+        res = findProfit(job,temp,n)+job[idx][2];
         if(temp-idx>1) 
-            res=max(res,findProfit(job,idx+1,n,memo));
-        return memo[idx] = res;
+            res=max(res,findProfit(job,idx+1,n));
+        return dp[idx] = res;
     }
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        memset(dp, -1, sizeof dp);
         int n = profit.size();
-        unordered_map<int,int> memo;
         vector<vector<int>> job(n,vector<int>(3));
         for(int i=0;i<n;i++){
             job[i][0] = startTime[i];
@@ -25,6 +26,6 @@ public:
             job[i][2] = profit[i];
         }
         sort(job.begin(),job.end(), cmp);
-        return findProfit(job, 0,n, memo);
+        return findProfit(job, 0,n);
     }
 };
