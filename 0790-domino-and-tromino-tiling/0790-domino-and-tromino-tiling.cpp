52837b54422dd1ba05667afad9fe55a29729c512
Gaurav Baweja <62277823+Aanvikshiki@users.noreply.github.com>
2022-12-24 18:48:57 +0530

    Time: 4 ms (44.75%), Space: 6.5 MB (51.09%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numTilings(int n) {
+        vector<long long>dp(n+1,0);
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 2;
+        if(n==3)
+            return 5;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        for(int i=4; i<=n; ++i) {
+            dp[i] = (2*dp[i-1] +dp[i-3]) % 1000000007;
+        }
+        return dp[n];
+    }
+};
