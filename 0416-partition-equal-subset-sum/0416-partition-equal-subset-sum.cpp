d9486eb0fe4da07d20a8773ce56301dc799c4a21
Gaurav Baweja <62277823+bawejagb@users.noreply.github.com>
2023-01-06 12:19:42 +0530

    Time: 21 ms (98.69%), Space: 9.6 MB (92.87%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,18 +1,16 @@
 class Solution {
-    bool findSum(vector<int>& nums, int sum){
-        vector<vector<bool>> dp(nums.size(),vector<bool>(sum+1,false));
-        for(int i=0;i<nums.size();i++)
-            dp[i][0]=true;
-        if(sum>nums[0])
-            dp[0][nums[0]]=true;
-        for(int i=1;i<nums.size();i++){
-            for(int j=1;j<=sum;j++){
-                dp[i][j] = dp[i-1][j];
-                if(nums[i]<=j)
-                    dp[i][j] = dp[i][j] | dp[i-1][j-nums[i]];
+    template <int N>
+    bool canPartitionN(vector<int>& nums, int target) {
+        using Set = bitset<N + 1>;
+        Set cur;
+        cur.set(0);
+        for (int val : nums) {
+            cur |= cur << val;
+            if (cur[target]) {
+                return true;
             }
         }
-        return dp[nums.size()-1][sum];
+        return false;
     }
 public:
     bool canPartition(vector<int>& nums) {
@@ -21,6 +19,6 @@ public:
             sm += num;
         if(sm%2) return false;
         sm /= 2;
-        return findSum(nums,sm);
+        return canPartitionN<10000>(nums, sm);
     }
 };
