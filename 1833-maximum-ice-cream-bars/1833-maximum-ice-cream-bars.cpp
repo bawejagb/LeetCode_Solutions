ece3fba634b286b61c40704f36b07bfb039e8426
Gaurav Baweja <62277823+bawejagb@users.noreply.github.com>
2023-01-06 11:42:06 +0530

    Time: 302 ms (44.73%), Space: 77 MB (7.08%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
+        int mxVal=0;
         int cnt=0;
+        int hmap[100005]{};
         for(int cst : costs){
-            if(cst>coins) break;
-            coins -= cst;
-            cnt++;
+            mxVal = max(cst, mxVal);
+            hmap[cst]++;
+        }
+        for(int i=1;i<=mxVal;i++){
+            if(hmap[i]==0) continue;
+            if(i>coins) break;
+            int mn = min(hmap[i],coins/i);
+            coins -= mn*i;
+            cnt += mn;
         }
         return cnt;
     }
