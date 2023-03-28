6125cb56fac888ef636618b9df3a9940d469653e
Gaurav Baweja <62277823+bawejagb@users.noreply.github.com>
2023-03-28 09:20:44 +0530

    Time: 4 ms (69.83%), Space: 9.7 MB (30.82%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -9,7 +9,7 @@ public:
             if (travelDays.find(i) == travelDays.end())
                 dp[i] = dp[i-1];
             else
-                dp[i] = min(dp[i-1] + costs[0], min(dp[max(0, i-7)] + costs[1], dp[max(0, i-30)] + costs[2]));
+                dp[i] = min({dp[i-1] + costs[0],dp[max(0, i-7)] + costs[1], dp[max(0, i-30)] + costs[2]});
         }
         return dp[365];
     }
