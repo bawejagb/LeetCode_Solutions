61e426d711604dce4ffa501240ed75e40caf1b47
Gaurav Baweja <62277823+bawejagb@users.noreply.github.com>
2023-03-30 08:25:39 +0530

    Time: 88 ms (65.25%), Space: 14.2 MB (81.67%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isScramble(string s1, string s2) {
         int n = s1.size();
-        vector dp(n + 1, vector(n, vector<int>(n,false)));
+        vector dp(n + 1, vector(n, vector<int>(n)));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 dp[1][i][j] = s1[i] == s2[j];
